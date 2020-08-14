446cc35c5736e0238294f35feec902fc7e5e4c0c
Alexander Brown <DrJKL0424@gmail.com>
2020-08-13 23:47:34 -0700

    Extract card from candidates

--- src/js/app.vue
+++ src/js/app.vue
@@ -2,39 +2,8 @@
   <main class="container">
     <div class="row">
       <div v-for="candidate of allCandidates" :key="candidate.name" class="col s4">
-        <div class="candidate-card card blue-grey z-depth-2">
-          <div class="card-content white-text">
-            <div class="card-title">{{candidate.name}}</div>
-            <div>
-              {{candidate.timer.getTimeLeft()}} {{candidate.timer.isTimeUp() ? 'over' :
-              'remaining'}}
-            </div>
-            <div class="spacer"></div>
-            <div>
-              <div class="progress" v-bind:class="{'time-up': candidate.timer.isTimeUp()}">
-                <div
-                  class="determinate"
-                  v-bind:style="{width: candidate.timer.getProgressPercent()}"
-                ></div>
-              </div>
-            </div>
-          </div>
-          <div class="card-action">
-            <a
-              href="#"
-              class="btn startstop"
-              v-on:click.prevent="candidate.timer.toggleTimer();"
-            >{{candidate.timer.isRunning() ? 'Stop': 'Start'}} Timer</a>
-            <div class="spacer"></div>
-
-            <a href="#" class="btn" v-on:click.prevent="candidate.timer.addTime()">
-              <i class="material-icons">add</i>
-            </a>
-            <a href="#" class="btn" v-on:click.prevent="candidate.timer.removeTime()">
-              <i class="material-icons">remove</i>
-            </a>
-          </div>
-        </div>
+        <candidate-card :candidate="candidate"></candidate-card>
+        
       </div>
     </div>
   </main>
@@ -43,8 +12,11 @@
 import { Component, Vue, Prop } from "vue-property-decorator";
 import moment from "moment";
 import { allCandidates as listOfCandidates } from "./candidates";
+import CandidateCard from "./candidate-card.vue";
 
-@Component({})
+@Component({
+  components: { CandidateCard },
+})
 export default class App extends Vue {
   allCandidates = listOfCandidates;
 }
---
+++ src/js/candidate-card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="candidate-card card blue-grey z-depth-2">
+    <div class="card-content white-text">
+      <div class="card-title">{{candidate.name}}</div>
+      <div>
+        {{candidate.timer.getTimeLeft()}} {{candidate.timer.isTimeUp() ? 'over' :
+        'remaining'}}
+      </div>
+      <div class="spacer"></div>
+      <div>
+        <div class="progress" v-bind:class="{'time-up': candidate.timer.isTimeUp()}">
+          <div class="determinate" v-bind:style="{width: candidate.timer.getProgressPercent()}"></div>
+        </div>
+      </div>
+    </div>
+    <div class="card-action">
+      <a
+        href="#"
+        class="btn startstop"
+        v-on:click.prevent="candidate.timer.toggleTimer();"
+      >{{candidate.timer.isRunning() ? 'Stop': 'Start'}} Timer</a>
+      <div class="spacer"></div>
+
+      <a href="#" class="btn" v-on:click.prevent="candidate.timer.addTime()">
+        <i class="material-icons">add</i>
+      </a>
+      <a href="#" class="btn" v-on:click.prevent="candidate.timer.removeTime()">
+        <i class="material-icons">remove</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { Candidate } from "./candidates";
+
+@Component({})
+export default class CandidateCard extends Vue {
+  @Prop({ required: true })
+  candidate: Candidate;
+}
+</script>
