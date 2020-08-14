7d8ae72e7d3980904105b10af1eba47268358c72
Alexander Brown <DrJKL0424@gmail.com>
2020-08-13 20:54:21 -0700

    Updates

--- vue-app.ddfc8642.js
+++ vue-app.ddfc8642.js
@@ -17882,7 +17882,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12683" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13457" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
