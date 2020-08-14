9f05b3a68d14deb0b6529675f1bbc125f2341ee8
Alexander Brown <DrJKL0424@gmail.com>
2020-08-14 00:13:16 -0700

    Use Buefy

--- src/js/vue-app.ts
+++ src/js/vue-app.ts
@@ -1,9 +1,13 @@
 import { allCandidates } from "./candidates";
 
 import Vue from "vue/dist/vue.js";
+import Buefy from 'buefy';
 import App from "./app.vue";
+
+import 'buefy/dist/buefy.css';
 
 new Vue({
   el: "#app",
   render: (h: (arg0: any) => any) => h(App),
 });
+Vue.use(Buefy);
