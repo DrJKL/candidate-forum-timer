37c818a463a8feda6dcc62156f0044f57583e2e6
Alexander Brown <DrJKL0424@gmail.com>
2020-08-14 20:16:53 -0700

    Updates

--- materialize.min.6aef4300.js
+++ materialize.min.6aef4300.js
@@ -6279,7 +6279,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10565" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1790" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
