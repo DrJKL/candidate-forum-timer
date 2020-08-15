2343cb0a4759362e1ef9f1f95e2deb909b7279c2
Alexander Brown <DrJKL0424@gmail.com>
2020-08-15 01:18:30 -0700

    Add Shuffling Button

--- src/js/app.vue
+++ src/js/app.vue
@@ -1,11 +1,16 @@
 <template>
-  <main class="container">
-    <div class="row">
-      <div v-for="candidate of allCandidates" :key="candidate.name" class="col s6 m4">
-        <candidate-card :candidate="candidate"></candidate-card>
+  <div>
+    <main class="container">
+      <div class="row">
+        <div v-for="candidate of allCandidates" :key="candidate.name" class="col s6 m4">
+          <candidate-card :candidate="candidate"></candidate-card>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
+    <section>
+      <a href="#" class="btn" v-on:click.prevent="shuffleCandidates()">Shuffle</a>
+    </section>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
@@ -18,6 +23,25 @@ import CandidateCard from "./candidate-card.vue";
 })
 export default class App extends Vue {
   allCandidates = listOfCandidates;
+
+  shuffleCandidates() {
+    const tempCandidates = this.allCandidates.slice();
+    let currentIndex = tempCandidates.length;
+    let temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = tempCandidates[currentIndex];
+      tempCandidates[currentIndex] = tempCandidates[randomIndex];
+      tempCandidates[randomIndex] = temporaryValue;
+    }
+    this.allCandidates = tempCandidates;
+  }
 }
 </script>
 <style lang="sass" scoped>
