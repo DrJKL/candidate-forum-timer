1ef257a561568d86192fb385da04658163479368
Alexander Brown <DrJKL0424@gmail.com>
2020-08-14 23:56:36 -0700

    Move Styles into component

--- src/js/candidate-card.vue
+++ src/js/candidate-card.vue
@@ -8,27 +8,24 @@
       </div>
       <div class="spacer"></div>
       <div>
-        <b-progress 
-          :value="progressPercent"
-          size="is-large"
-          type="is-info"
-          ></b-progress>
+        <b-progress :value="progressPercent" size="is-large" type="is-info"></b-progress>
       </div>
     </div>
-    <div class="card-action">
+    <div class="card-action rows">
       <a
         href="#"
-        class="btn startstop"
+        class="btn startstop col"
         v-on:click.prevent="candidate.timer.toggleTimer();"
-      >{{candidate.timer.isRunning() ? 'Stop': 'Start'}} Timer</a>
-      <div class="spacer"></div>
-
+      >{{candidate.timer.isRunning() ? 'Stop': 'Start'}}</a>
+      <div class="spacer col"></div>
+      <div class="col">
       <a href="#" class="btn" v-on:click.prevent="candidate.timer.addTime()">
         <i class="material-icons">add</i>
       </a>
       <a href="#" class="btn" v-on:click.prevent="candidate.timer.removeTime()">
         <i class="material-icons">remove</i>
       </a>
+      </div>
     </div>
   </div>
 </template>
@@ -41,9 +38,50 @@ import { Candidate } from "./candidates";
 export default class CandidateCard extends Vue {
   @Prop({ required: true })
   candidate!: Candidate;
-  
+
   get progressPercent() {
     return this.candidate.timer.progressPercent;
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.card-content .timer {
+    width: 100%;
+    height: 1em;
+    background-color: blanchedalmond;
+}
+
+.candidate-card .card-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.candidate-card .card-title {
+    overflow: hidden;
+    word-wrap: normal;
+    white-space: nowrap;
+}
+
+.candidate-card .card-action {
+    display: flex;
+}
+.candidate-card .card-action > * {
+    flex: 0 1 auto;
+}
+.candidate-card .card-action > .spacer {
+    flex-grow: 1;
+}
+
+.time-up.progress {
+    background-color: red;
+}
+
+.candidate-card .progress {
+    height: 8px;
+}
+
+.startstop.btn {
+    width: 9em;
+}
+</style>
