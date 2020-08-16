fb0d067f492ce232a69907be705202011f0cbb87
Alexander Brown <DrJKL0424@gmail.com>
2020-08-16 15:10:23 -0700

    Add global time buttons.

--- src/js/app.vue
+++ src/js/app.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <main class="container">
+      <section class="buttons">
+        <a href="#" class="btn" v-on:click.prevent="shuffleCandidates()">Shuffle</a>
+
+        <div class="time-setters-global">
+          <a href="#" class="btn" v-on:click.prevent="setTime(30)">30</a>
+          <a href="#" class="btn" v-on:click.prevent="setTime(60)">60</a>
+          <a href="#" class="btn" v-on:click.prevent="setTime(90)">90</a>
+        </div>
+      </section>
       <div class="candidates-container">
-        <div v-for="candidate of allCandidates" :key="candidate.name" class="">
+        <div v-for="candidate of allCandidates" :key="candidate.name" class>
           <candidate-card :candidate="candidate"></candidate-card>
         </div>
       </div>
     </main>
-    <section>
-      <a href="#" class="btn" v-on:click.prevent="shuffleCandidates()">Shuffle</a>
-    </section>
   </div>
 </template>
 <script lang="ts">
@@ -42,15 +48,34 @@ export default class App extends Vue {
     }
     this.allCandidates = tempCandidates;
   }
+
+  setTime(time: number) {
+    this.allCandidates
+      .map((candidate) => candidate.timer)
+      .forEach((timer) => {
+        timer.setTime(time, "s");
+      });
+  }
 }
 </script>
 <style lang="scss" scoped>
+.buttons {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: flex-end;
+  margin: 0;
+  a {
+    flex: 0 1 auto;
+  }
+}
+
 .candidates-container {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: space-evenly;
   > div {
-    margin: 0 .5em;
+    margin: 0 0.5em;
   }
 }
 </style>
