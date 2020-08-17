ab89d9cf597f01abe076caec05636b5ca4492728
Alexander Brown <DrJKL0424@gmail.com>
2020-08-16 17:28:27 -0700

    Extract timer, add minimize property.

--- src/js/candidates.ts
+++ src/js/candidates.ts
@@ -1,70 +1,21 @@
 import Config from './global_config';
 import moment from 'moment';
 import {DurationInputArg1, DurationInputArg2} from 'moment';
+import Timer from './timer';
 
 export class Candidate {
-    public readonly timer: Timer;
+    public readonly timer: Timer = new Timer();
+    public isMinimized = false;
 
     constructor(public readonly name: string) {
         this.name = name;
-        this.timer = new Timer();
     }
-
-}
-
-export class Timer {
-    public timeLeft: moment.Duration;
-    public countingDown: number|null;
-
-    constructor() {
-        this.timeLeft = Config.timeLimitTotal.clone();
-        this.countingDown = null;
-    }
-
-    toggleTimer() {
-        if (this.countingDown) {
-            clearInterval(this.countingDown);
-            this.countingDown = null;
-            return;
-        }
-        this.countingDown = setInterval(() => {
-            this.timeLeft.subtract(Config.timeGranularity, 'ms');
-        }, Config.timeGranularity);
-    }
-
-    addTime() {
-        this.timeLeft.add(Config.timeDelta);
-    }
-
-    removeTime() {
-        this.timeLeft.subtract(Config.timeDelta);
-    }
-
-    setTime(num: DurationInputArg1, unit: DurationInputArg2) {
-        this.timeLeft = moment.duration(num, unit);
-    }
-
-    isRunning() { return !!this.countingDown; }
-
-    getTimeLeft() {
-        const timeToUse = this.isTimeUp ? moment.duration().subtract(this.timeLeft) : this.timeLeft;
-        return `${timeToUse.minutes().toString().padStart(2, '0')}:` +
-            `${timeToUse.seconds().toString().padStart(2, '0')}.` +
-            `${timeToUse.milliseconds().toString().substr(0,1)}`;
-    }
-
-    get millisLeft() {
-        return this.timeLeft.asMilliseconds();
-    }
-
-    get progressPercent() {
-        return 100*(this.millisLeft / Config.timeLimitTotal.asMilliseconds());
-    }
-
-    get isTimeUp() {
-        return this.millisLeft <= 0;
+    toggleMinimized() {
+        this.isMinimized = !this.isMinimized;
     }
 }
+
+
 
 export const allCandidates = [
     'Margaret Abe-Koga',
---
+++ src/js/timer.ts
@@ -0,0 +1,53 @@
+export default class Timer {
+    public timeLeft: moment.Duration;
+    public countingDown: number|null;
+
+    constructor() {
+        this.timeLeft = Config.timeLimitTotal.clone();
+        this.countingDown = null;
+    }
+
+    toggleTimer() {
+        if (this.countingDown) {
+            clearInterval(this.countingDown);
+            this.countingDown = null;
+            return;
+        }
+        this.countingDown = setInterval(() => {
+            this.timeLeft.subtract(Config.timeGranularity, 'ms');
+        }, Config.timeGranularity);
+    }
+
+    addTime() {
+        this.timeLeft.add(Config.timeDelta);
+    }
+
+    removeTime() {
+        this.timeLeft.subtract(Config.timeDelta);
+    }
+
+    setTime(num: DurationInputArg1, unit: DurationInputArg2) {
+        this.timeLeft = moment.duration(num, unit);
+    }
+
+    isRunning() { return !!this.countingDown; }
+
+    getTimeLeft() {
+        const timeToUse = this.isTimeUp ? moment.duration().subtract(this.timeLeft) : this.timeLeft;
+        return `${timeToUse.minutes().toString().padStart(2, '0')}:` +
+            `${timeToUse.seconds().toString().padStart(2, '0')}.` +
+            `${timeToUse.milliseconds().toString().substr(0,1)}`;
+    }
+
+    get millisLeft() {
+        return this.timeLeft.asMilliseconds();
+    }
+
+    get progressPercent() {
+        return 100*(this.millisLeft / Config.timeLimitTotal.asMilliseconds());
+    }
+
+    get isTimeUp() {
+        return this.millisLeft <= 0;
+    }
+}
