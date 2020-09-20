cc887864cef0f8e6ead9cbfffc6246aaca24a765
Alexander Brown <DrJKL0424@gmail.com>
2020-09-19 21:01:28 -0700

    Updates

--- materialize.02861d82.js
+++ materialize.02861d82.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7435" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12350" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
