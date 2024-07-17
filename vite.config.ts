661239d9488d850df6284df63f9b9467b95271ab
DrJKL <DrJKL0424@gmail.com>
2024-07-16 18:28:23 -0700

    Turn off compatibility mode.

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,7 @@ export default defineConfig({
     plugins: [
         vue({
             template: {
-              compilerOptions: {
-                compatConfig: {
-                  MODE: 3
-                }
-              }
+              compilerOptions: {}
             }
           })
     ]
