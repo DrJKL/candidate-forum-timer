561874e1c71d728db3516496aa6fb7e99cf1e2b4
DrJKL <DrJKL0424@gmail.com>
2024-07-22 18:27:00 -0700

    Remove compat

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    resolve: {
-      alias: {
-        vue: '@vue/compat'
-      }
-    },
-    plugins: [
-        vue({
-            template: {
-              compilerOptions: {}
-            }
-          })
-    ]
-  });
+
+  plugins: [
+    vue(),
+  ],
+});
--- vue.shims.d.ts
+++ vue.shims.d.ts
@@ -1,14 +1,6 @@
-
-declare module 'vue' {
-  import { CompatVue } from '@vue/runtime-dom'
-  const Vue: CompatVue
-  export default Vue
-  export * from '@vue/runtime-dom'
-  const { configureCompat } = Vue
-  export { configureCompat }
-}
-declare module '*.vue' {
-    import Vue from 'vue'
-    export default Vue
-  }
 export {}
+
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
